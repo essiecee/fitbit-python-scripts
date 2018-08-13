10398779a6cb8fe00e054b0d1d8bb1447303d169
Esther Chien <33583198+essiecee@users.noreply.github.com>
2018-08-12 22:45:33 -0400

    Update tractdbsession2.py

--- tractdbsession2.py
+++ tractdbsession2.py
@@ -4,6 +4,7 @@ import base64
 
 # token renewal
 session_fitbit = requests.session()
+
 response = session_fitbit.post(
     'https://api.fitbit.com/oauth2/token',
     headers={
@@ -16,15 +17,14 @@ response = session_fitbit.post(
     },
     data={
         'grant_type': 'refresh_token',
-        'refresh_token': '1ba7724ac3e501a94deb8dc6be3d1ee2ac3ffef0fb8895645efc16db5e47ece8',
+        'refresh_token': '7990197c0b6dc5238ff2c869c530f0df2097eb8a87255dd34e94dfc9599594a2',
         'redirect_uri': 'https://tractdb.org/configure/fitbit/callback'
     }
 )
-
+# prints out a new set of tokens that can be used (access and refresh tokens)
 print response.text
 
 # part 2
-# logging in
 login = session_fitbit.post(
     '{}/{}'.format(
         'https://tractdb.org/api',
@@ -51,19 +51,16 @@ fitbittokens_by_id = {
 
 doc_decoded["fitbit_tokens"] = list(fitbittokens_by_id.values())
 
-"""
 update = session_fitbit.put(
    "{}/{}/{}".format(
-      "https://tractdb.org/api"
+      "https://tractdb.org/api",
       "document",
       "fitbit_tokens"
    ),
    json=doc_decoded
 )
-"""
-# this works, but not the format fogarty wrote- the commented out section
-update = session_fitbit.put("https://tractdb.org/api/document/fitbit_tokens", json=doc_decoded)
 
+# uses the new access token
 activitylogs = session_fitbit.get(
    "https://api.fitbit.com/1/user/{}/{}/date/{}/{}.json".format(
       "6Q9B5C",
@@ -73,7 +70,7 @@ activitylogs = session_fitbit.get(
    ),
    headers={
       'Authorization':'Bearer {}'.format(
-            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTMzNjAzNDUwLCJpYXQiOjE1MzM1NzQ2NTB9.R0pmd7zf8cBvw9sZBd8Ye0CCnwEfGo7In8hzeKHv3mc'
+            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTMzODE3OTk0LCJpYXQiOjE1MzM3ODkxOTR9.Qqok-vwon0niUdw18ZDiqrephclGAw6Oko58Z2QmYmk'
       )
    }
 )
