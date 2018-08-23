a7baf12aa9e3c2aa3a773d775ecda4fc918280fa
Esther Chien <ecmango24@gmail.com>
2018-08-22 19:38:08 -0700

    Update tractdbsession3.py

--- tractdbsession3.py
+++ tractdbsession3.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import base64
+from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 # token renewal
 session_fitbit = requests.session()
@@ -17,14 +20,14 @@ response = session_fitbit.post(
     },
     data={
         'grant_type': 'refresh_token',
-        'refresh_token': '7990197c0b6dc5238ff2c869c530f0df2097eb8a87255dd34e94dfc9599594a2',
+        'refresh_token': '02082c3183c4581a775402ad17d9bc08906b57ee0bfd7cfdcd8572196fa6aaf7',
         'redirect_uri': 'https://tractdb.org/configure/fitbit/callback'
     }
 )
 # prints out a new set of tokens that can be used (access and refresh tokens)
 print response.text
 
-# part 2
+# logging in
 login = session_fitbit.post(
     '{}/{}'.format(
         'https://tractdb.org/api',
@@ -39,9 +42,23 @@ first_active = session_fitbit.get(
    ),
    headers={
       'Authorization':'Bearer {}'.format(
-            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTMzODE3OTk0LCJpYXQiOjE1MzM3ODkxOTR9.Qqok-vwon0niUdw18ZDiqrephclGAw6Oko58Z2QmYmk'
+            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNTM1MDE5NTYxLCJpYXQiOjE1MzQ5OTA3NjF9.oB0rxKx-CX_sr3y4yrY2xHLzZxcFPweC0u3OsKuDFyM'
       )
    }
 )
-print ("status code is %s" % first_active.status_code)
-print first_active.json()["user"]["memberSince"]
+   
+memberStart = first_active.json()["user"]["memberSince"]
+
+print("user has been a member since %s" % memberStart)
+# memberStart is currently in the format of 2018-06-22, a string
+
+today = date.today();
+print("today's date is %s" % today)
+
+# printing out the months between memberSince and today 
+
+cur_date = datetime.strptime(memberStart, '%Y-%m-%d').date()
+
+while cur_date < today:
+   print(cur_date.strftime("%m/%y"))
+   cur_date += relativedelta(months=1)
