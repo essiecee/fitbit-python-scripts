acd812c4829dc078faa59bf730d63ce6c9b91b89
Esther Chien <ecmango24@gmail.com>
2018-09-01 21:38:00 -0700

    Update tractdbsession3.py

--- tractdbsession3.py
+++ tractdbsession3.py
@@ -1,10 +1,8 @@
 import json
 import requests
 import base64
-from datetime import date
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from datetime import timedelta
+from datetime import timedelta, date, datetime
 
 # token renewal
 session_fitbit = requests.session()
@@ -21,21 +19,12 @@ response = session_fitbit.post(
     },
     data={
         'grant_type': 'refresh_token',
-        'refresh_token': '4b2c6f71c6f8d0a3873897225f56ae4cef6d326193ae831f95d0b252a8719486',
+        'refresh_token': 'afa3a6319b45623d58cba0daa758068c85ac837975a31861ae0a7348d9aefe28',
         'redirect_uri': 'https://tractdb.org/configure/fitbit/callback'
     }
 )
 # prints out a new set of tokens that can be used (access and refresh tokens)
 print response.text
-
-# logging in
-login = session_fitbit.post(
-    '{}/{}'.format(
-        'https://tractdb.org/api',
-        'login'
-    ),
-    json = {'account': 'essiecee', 'password': 'zxczxc'}
-)
 
 first_active = session_fitbit.get(
    "https://api.fitbit.com/1/user/{}/profile.json".format(
@@ -43,7 +32,7 @@ first_active = session_fitbit.get(
    ),
    headers={
       'Authorization':'Bearer {}'.format(
-            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNTM1MDcwNTI5LCJpYXQiOjE1MzUwNDE3Mjl9.-lyyOkkJEmD_H2MagmXLavOLOlPWg6aysYhSdOnzTUs'
+            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTM1ODg2MzEyLCJpYXQiOjE1MzU4NTc1MTJ9.K68j_crvzUscwi_OuJ6zxlsmeRTDMTMmYwlxPjdngks'
       )
    }
 )
@@ -56,27 +45,33 @@ today = date.today()
 print("today's date is %s" % today)
 
 # printing out the months between memberSince and today 
-
+memberStart_datetime = datetime.strptime(memberStart, '%Y-%m-%d').date()
 cur_date = datetime.strptime(memberStart, '%Y-%m-%d').date()
 
 while cur_date.month < today.month:
-   print(cur_date.strftime("%m/%y"))
-   end_date = cur_date + relativedelta(months=1)
-
+   print(cur_date.strftime("%m/%Y"))
+   if cur_date.month == memberStart_datetime.month:
+      beginning_date_day = memberStart_datetime.day
+      next_month = memberStart_datetime.replace(day=28) + timedelta(days=4)
+   else:
+      end_date = cur_date + relativedelta(months=1)
+      beginning_date_day = "01"
+      next_month = cur_date.replace(day=28) + timedelta(days=4)
+   end_date = next_month - timedelta(days=next_month.day)
    month_summary = session_fitbit.get(
-      "https://api.fitbit.com/1/user/{}/{}/date/{}/{}.json".format(
+      "https://api.fitbit.com/1/user/{}/{}/date/{}-0{}-{}/{}.json".format(
          "6Q9B5C",
          "activities/calories",
-         cur_date,
-         end_date
+          cur_date.year,
+          cur_date.month,
+          beginning_date_day,
+          end_date
       ),
       headers={
       'Authorization':'Bearer {}'.format(
-            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNTM1MDcwNTI5LCJpYXQiOjE1MzUwNDE3Mjl9.-lyyOkkJEmD_H2MagmXLavOLOlPWg6aysYhSdOnzTUs'
+            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2UTlCNUMiLCJhdWQiOiIyMjhSWTkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTM1ODg2MzEyLCJpYXQiOjE1MzU4NTc1MTJ9.K68j_crvzUscwi_OuJ6zxlsmeRTDMTMmYwlxPjdngks'
          )
       }
    )
-   for x in range(0, 31): 
-      print(month_summary.json()["activities-calories"][x])
-   
+   print month_summary.text   
    cur_date += relativedelta(months=1)
